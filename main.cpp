d165b703ae0baba6668d8211153abc1eef100db4
AsgerSuhr <asger31@gmail.com>
2022-06-18 13:42:58 +0200

    removing unnecessary module

--- main.cpp
+++ main.cpp
@@ -1,21 +1,43 @@
-#include <iostream>
+#include <stdio.h>
+#include <cstring>
 #include "pico/stdlib.h"
-#include "ESP8266WiFi.h"
+#include "hardware/uart.h"
 
-int main(int, char**) {
-    const uint led_pin = 25;
-    const uint relay_pin = 0;
+#define UART_ID uart0 
+#define BAUD_RATE 115200
+#define LED_PIN 25
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+int main() {
+    // Set up uart with the right baud rate, ESP8266 default is 115200
+    uart_init(UART_ID, BAUD_RATE);
+
+    //Set the GPIO pins function to be UART
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+    //You can use printf to send out data through the uart, 
+    //but there are other functions like:
     
-    gpio_init(led_pin);
-    gpio_set_dir(led_pin, GPIO_OUT);
-    gpio_init(relay_pin);
-    gpio_set_dir(relay_pin, GPIO_OUT);
+    // Send out character without any conversion
+    //uart_putc_raw(UART_ID, 'A');
 
-    for (int i=0; i<2; i++) {
+    // Send out character but do CR/LF conversions
+    //uart_putc(UART_ID, 'A');
 
-        gpio_put(relay_pin, false);
-        sleep_ms(1000);
-        gpio_put(relay_pin, true);
-        sleep_ms(1000);
+    // Send out string with CR/LF conversion
+    //uart_puts(UART_ID, "AT");
+
+
+    char buffer[1024];
+    //sleep_ms(10000);
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    
+    while (true) {
+        uart_puts(UART_ID, "AT\r\n");
+        sleep_ms(10000);
     }
 }
